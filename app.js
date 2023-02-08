f7b11d60d3a0cb4f7b75511fd8234548f0716ab1
naznin2345 <121008080+naznin2345@users.noreply.github.com>
2023-02-08 09:24:59 +0600

    e

---
+++ app.js
@@ -0,0 +1,276 @@
+const express = require("express");
+const app = express();
+var pdf = require("html-pdf");
+
+const options = {
+    format: "A3",
+    orientation: "landscape",
+    margin: {
+        top: "20px",
+        right: "20px",
+        bottom: "20px",
+        left: "20px",
+    },
+};
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+const html = (Data) => {
+    return `
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+        <title>DemoPdf</title>
+
+        <style>
+        table {
+            width: 99%;
+        }
+        tr {
+            text-align: center;
+        }
+        td {
+            text-align: center;
+            padding: 5px;
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+        table,
+        tr,
+        th {
+            padding: 10px;
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+        h1 {
+            font-size: 50px;
+            font-family: Tahoma;
+        }
+        h2 {
+            font-family: Tahoma;
+        }
+        th {
+            font-family: Tahoma;
+        }
+    </style>
+    </head>
+
+    <body>
+
+        <div>
+
+
+        <caption>
+        </br>
+        <h1 style="text-align:center">Leading University</h1>
+        <h2 style="text-align:left">&nbsp;&nbsp;&nbsp;&nbsp;Attendence Sheet</br>
+            &nbsp;&nbsp;&nbsp; Program: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            Batch: &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            Course code: &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+
+            Course Title: &nbsp;
+
+
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            Faculty Name: &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+
+            Semester: &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+
+        </h2></br>
+    </caption>
+        </div>
+
+        <center>
+            <table "border="1" align="center" cellspacing="0"id="tb1">
+
+
+                <tr>
+
+                    <th>Student ID</th>
+                    <th>Student Name</th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th><th></th><th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th><th></th><th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th><th></th><th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th><th></th><th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th><th></th><th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th><th></th><th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th><th></th><th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th><th></th><th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th><th></th><th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th><th></th>
+
+                </tr>
+
+               ${Data.map(
+                   (d, i) =>
+                       ` <tr>
+                        <td>${d.CurriculumID}.</td>
+                        <td> ${d.CourseCode} </td>
+                        <td></td>
+                        <td></td><td></td>
+                        <td></td><td></td>
+                        <td></td><td></td>
+                        <td></td><td></td>
+                        <td></td><td></td>
+                        <td></td><td></td>
+                        <td></td><td></td>
+                        <td></td><td></td>
+                        <td></td><td></td>
+                        <td></td><td></td>
+                        <td></td><td></td>
+                        <td></td><td></td>
+                        <td></td><td></td>
+                        <td></td><td></td>
+                        <td></td><td></td>
+                        <td></td><td></td>
+                        <td></td><td></td>
+                        <td></td><td></td>
+                        <td></td><td></td>
+                        <td></td><td></td>
+                        <td></td><td></td>
+                        <td></td><td></td>
+                        <td></td><td></td>
+                        <td></td><td></td>
+                        <td></td>
+                    </tr>
+                `
+               )
+                   .toString()
+                   .split(",")
+                   .join("")}
+
+
+
+
+            </table>
+        </center>
+
+    </body>
+
+    </html>
+    `;
+};
+
+app.post("/", async (req, res) => {
+    pdf.create(html(req.body), options).toFile(
+        "./Curriculum.pdf",
+        (err, result) => {
+            if (err) {
+                res.status(404).json({ message: "Failed" });
+            } else {
+                res.status(200).json({ message: "Successfully created" });
+            }
+        }
+    );
+});
+
+app.listen(3030, () => {
+    console.log("Server started");
+});
